cdc1b4b4be2575da017cd5900c441c1f4bca0cb7
opariffazman <ariff.azman@proton.me>
2023-06-21 12:45:11 +0800

    feat: initial commit of automated profile update

---
+++ index.js
@@ -0,0 +1,26 @@
+// index.js
+const Mustache = require("mustache")
+const fs = require("fs")
+const MUSTACHE_MAIN_DIR = "./main.mustache"
+
+let DATA = {
+  name: "opariffazman",
+  date: new Date().toLocaleDateString("en-GB", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZoneName: "short",
+    timeZone: "Asia/Singapore",
+  }),
+}
+
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err
+    const output = Mustache.render(data.toString(), DATA)
+    fs.writeFileSync("README.md", output)
+  })
+}
+generateReadMe()
