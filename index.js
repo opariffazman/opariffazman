0a6fe741b78cf1925614994ada0886cea425a094
opariffazman <ariff.azman@proton.me>
2025-06-23 14:05:02 +0800

    feat: added <head> elements

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ let DATA = {
     timeZoneName: "short",
     timeZone: "Asia/Singapore",
   }),
+  favicons: "<!DOCTYPE html>\n<html lang=\"en\">\n<!-- Favico -->\n<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"images/apple-touch-icon.png\">\n<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"images/favicon-32x32.png\">\n<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"images/favicon-16x16.png\">\n<meta charset=\"utf-8\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<link rel=\"icon\" type=\"image/png\" href=\"data:image/png;base64,\">"
 }
 
 function generateReadMe() {
