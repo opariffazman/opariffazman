589b458a232dbc1bd0ed20742d8b56ff86348cb9
opariffazman <ariff.azman@proton.me>
2025-06-23 14:37:00 +0800

    feat: added support for dynamic file output

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs")
 const MUSTACHE_MAIN_DIR = "./main.mustache"
 
 let DATA = {
+  isHtml: false,
   name: "opariffazman",
   nickname: "opsedar",
   discordUserId: "178370555072872449",
@@ -20,11 +21,12 @@ let DATA = {
   favicons: "<!DOCTYPE html>\n<html lang=\"en\">\n<!-- Favico -->\n<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"images/apple-touch-icon.png\">\n<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"images/favicon-32x32.png\">\n<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"images/favicon-16x16.png\">\n<meta charset=\"utf-8\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<link rel=\"icon\" type=\"image/png\" href=\"data:image/png;base64,\">"
 }
 
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+function generateFile(outFile, isHtml) {
+  const data = { ...DATA, isHtml }
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, template) => {
     if (err) throw err
-    const output = Mustache.render(data.toString(), DATA)
-    fs.writeFileSync("README.md", output)
+    const output = Mustache.render(template.toString(), data)
+    fs.writeFileSync(outFile, output)
   })
 }
-generateReadMe()
+generateFile("README.md", false)
